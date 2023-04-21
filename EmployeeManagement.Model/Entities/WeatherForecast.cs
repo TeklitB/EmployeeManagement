e4d98aa93d29909ec4d2152f607fb649adf0387c
Teklit Berihu Gereziher <teklit.berihu.gereziher@diadrom.se>
2023-04-21 17:25:24 +0200

    Remove nullable from already nullable type

--- EmployeeManagement.Model/Entities/WeatherForecast.cs
+++ EmployeeManagement.Model/Entities/WeatherForecast.cs
@@ -5,6 +5,6 @@ namespace EmployeeManagement.Model.Entities
         public DateTime Date { get; set; }
         public int TemperatureC { get; set; }
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-        public string? Summary { get; set; }
+        public string Summary { get; set; }
     }
 }
